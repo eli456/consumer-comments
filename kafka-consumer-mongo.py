979dd7d2ef3fe4feee32773449eb5641d0a26a58
adsoftsito <adsoft@live.com.mx>
2023-05-21 22:13:06 -0600

    Update kafka-consumer-mongo.py

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -71,7 +71,7 @@ for msg in consumer:
 
        for i in agg_result:
          print(i)
-       db.memes_summary.insert_many(json.loads(agg_result))
+       db.memes_summary.insert_many(json.dumps(json.loads(agg_result)))
        
     except Exception as e:
        print(f'group by caught {type(e)}: ')
