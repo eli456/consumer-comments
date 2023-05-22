4075ebc085001d60141306ad564df62ff258da8d
adsoftsito <adsoft@live.com.mx>
2023-05-21 22:39:16 -0600

    Update kafka-consumer-mongo.py

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -68,7 +68,7 @@ for msg in consumer:
             "n"    : {"$sum": 1}
          }}
        ])
-       db.memes_summary.remove({})
+       db.memes_summary.deleteMany({})
        for i in agg_result:
          print(i)
          summary_id = db.memes_summary.insert_one(i)
