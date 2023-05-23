cb5afcd391547c7a758d3dede45fdd0d0418a759
eli456 <eli456pg@gmail.com>
2023-05-22 23:33:04 -0600

    inicializar

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -11,7 +11,7 @@ import subprocess
 
 
 # replace here with your mongodb url 
-uri = "mongodb+srv://adsoft:adsoft-sito@cluster0.kzghgph.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://eli456:sag<3@nosql.hcxakbs.mongodb.net/?retryWrites=true&w=majority"
 
 
 # Create a new client and connect to the server
@@ -31,13 +31,13 @@ try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 
-    db = client.memes
+    db = client.nosql
     print("MongoDB Connected successfully!")
 except:
     print("Could not connect to MongoDB")
 
 consumer = KafkaConsumer('test',bootstrap_servers=[
-     'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'
+     'my-kafka-0.my-kafka-headless.eli456.svc.cluster.local:9092'
     ])
 # Parse received data from Kafka
 for msg in consumer:
@@ -47,34 +47,12 @@ for msg in consumer:
 
     # Create dictionary and ingest data into MongoDB
     try:
-       meme_rec = {'name':name }
-       print (meme_rec)
-       meme_id = db.memes_info.insert_one(meme_rec)
-       print("Data inserted with record ids", meme_id)
+       nosql_rec = {'name':name }
+       print (nosql_rec)
+       nosql_id = db.nosql_info.insert_one(nosql_rec)
+       print("Data inserted with record ids", nosql_id)
 
-       #subprocess.call(['sh', './test.sh'])
+       subprocess.call(['sh', './test.sh'])
 
     except:
        print("Could not insert into MongoDB")
-    
-    
-
-    # Create dictionary and ingest data into MongoDB
-    try:
-       agg_result= db.memes_info.aggregate(
-       [{
-         "$group" : 
-         {  "_id" : "$name", 
-            "n"    : {"$sum": 1}
-         }}
-       ])
-       db.memes_summary.delete_many({})
-       for i in agg_result:
-         print(i)
-         summary_id = db.memes_summary.insert_one(i)
-         print("Summary inserted with record ids", summary_id)
-
-    except Exception as e:
-       print(f'group by caught {type(e)}: ')
-       print(e)
-    
